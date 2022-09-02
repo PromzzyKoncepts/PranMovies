df1cd07df9170a913e06ad8f144701b5d92c5c57
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-02 10:25:55 +0100

    implemented the add like function

--- modules/homepage.js
+++ modules/homepage.js
@@ -1,4 +1,5 @@
 import { fetchLikeApi, submitNewLike } from './likes.js'
+
 const movieApi = async () => {
   const fetchResult = await fetch('https://api.tvmaze.com/shows');
   const ShowResult = await fetchResult.json();
@@ -18,8 +19,8 @@ const movieList = async () => {
     let cardLikes = fetchLikes.find((like) => like.item_id === card.id);
     const id = card.id;
 
-
- 
+    
+    let livecount = +cardLikes?.likes;
 
     const CardContainer = document.querySelector('.card-container');
     const cardUL = document.createElement('ul');
@@ -32,37 +33,21 @@ const movieList = async () => {
                         <a href="${card.officialSite}" class="movie-title">${card.name}</a>
                         <div class="movie-info">
                           <p>${card.weight}mb</p>
-                          <i class="fa fa-heart" id = "${card.id}" aria-hidden="true"></i>
-                          <p class = "totalLikes">${cardLikes?.likes} likes</p>
+                          <i class="fa fa-heart"  aria-hidden="true"></i>
+                          <p class = "totalLikes">${livecount} likes</p>
                         </div>
                         <button>Comments</button>`;
     
-    let cardLikeNum = +cardLikes?.likes;
-    const liveCount = (e) => {
-      // let cardLikeNum = parseInt(cardLikeNum, 10)
-      cardLikeNum++
-       if (e.target.id === id) {
-        const totalLikes = document.querySelector('.totalLikes')
-        totalLikes.innerHTML = cardLikeNum
-      }      
-      const totalLikes = document.querySelector('.totalLikes')
-      totalLikes.innerHTML = cardLikeNum
-      console.log('>>>>', typeof (cardLikeNum));
-    }
     
     
-    // const likeButton = cardLI.getElementsByClassName('fa-heart')[0];
-    const likeButton = document.querySelectorAll('.fa-heart');
-    likeButton.forEach((like) => {
-      like.addEventListener('click', (e) => {
-        // movieList()
-        submitNewLike(id);
-        liveCount(e)
-        // if (e.target.id === id) {
-        //   const totalLikes = document.querySelector('.totalLikes')
-        //   totalLikes.innerHTML = cardLikeNum
-        // }      
-      })
+    const likeButton = cardLI.getElementsByClassName('fa-heart')[0];
+    likeButton.addEventListener('click', async () => {
+      const liveCountElement = cardLI.getElementsByClassName('totalLikes')[0];
+      livecount += 1;
+      liveCountElement.innerHTML = `${livecount} likes`;
+      submitNewLike(id);
+
+      console.log('>>>>',livecount);
     })
  
 
@@ -73,7 +58,6 @@ const movieList = async () => {
     const totalLikes = document.querySelectorAll('.totalLikes');
     totalLikes.forEach((like) => {
       if (like.innerText == 'undefined likes') {
-        // const likesrd =  like.innerText.slice(10,15))
         like.innerText = '0 like';
       }
       else if (like.innerText == '1 likes') {
